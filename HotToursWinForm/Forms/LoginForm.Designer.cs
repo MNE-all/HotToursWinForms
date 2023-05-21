bd2dfedd8a7d4829cd60434fc19b8eb3553d0ce1
Николай Монашов <monashovnikolaye@gmail.com>
2023-05-22 00:05:29 +0300

    bind

--- HotToursWinForm/Forms/LoginForm.Designer.cs
+++ HotToursWinForm/Forms/LoginForm.Designer.cs
@@ -28,82 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            buttonEnter = new Button();
-            label2 = new Label();
-            textBoxLogin = new TextBox();
-            textBoxPassword = new TextBox();
-            label3 = new Label();
-            SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(59, 129);
-            label1.Name = "label1";
-            label1.Size = new Size(41, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Логин";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин";
             // 
             // buttonEnter
             // 
-            buttonEnter.Location = new Point(137, 225);
-            buttonEnter.Name = "buttonEnter";
-            buttonEnter.Size = new Size(75, 23);
-            buttonEnter.TabIndex = 1;
-            buttonEnter.Text = "Войти";
-            buttonEnter.UseVisualStyleBackColor = true;
-            buttonEnter.Click += buttonEnter_Click;
+            this.buttonEnter.Location = new System.Drawing.Point(157, 300);
+            this.buttonEnter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(86, 31);
+            this.buttonEnter.TabIndex = 1;
+            this.buttonEnter.Text = "Войти";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(59, 167);
-            label2.Name = "label2";
-            label2.Size = new Size(49, 15);
-            label2.TabIndex = 2;
-            label2.Text = "Пароль";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пароль";
             // 
             // textBoxLogin
             // 
-            textBoxLogin.Location = new Point(124, 126);
-            textBoxLogin.Name = "textBoxLogin";
-            textBoxLogin.Size = new Size(151, 23);
-            textBoxLogin.TabIndex = 3;
+            this.textBoxLogin.Location = new System.Drawing.Point(142, 168);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(172, 27);
+            this.textBoxLogin.TabIndex = 3;
             // 
             // textBoxPassword
             // 
-            textBoxPassword.Location = new Point(124, 164);
-            textBoxPassword.Name = "textBoxPassword";
-            textBoxPassword.Size = new Size(151, 23);
-            textBoxPassword.TabIndex = 4;
+            this.textBoxPassword.Location = new System.Drawing.Point(142, 219);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(172, 27);
+            this.textBoxPassword.TabIndex = 4;
             // 
             // label3
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(137, 41);
-            label3.Name = "label3";
-            label3.Size = new Size(78, 15);
-            label3.TabIndex = 5;
-            label3.Text = "Авторизация";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Авторизация";
             // 
             // LoginForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(353, 292);
-            Controls.Add(label3);
-            Controls.Add(textBoxPassword);
-            Controls.Add(textBoxLogin);
-            Controls.Add(label2);
-            Controls.Add(buttonEnter);
-            Controls.Add(label1);
-            Name = "LoginForm";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Горячие туры | Авторизация";
-            FormClosed += LoginForm_FormClosed;
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 389);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Горячие туры | Авторизация";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.LoginForm_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
